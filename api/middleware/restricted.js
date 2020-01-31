34dddc2f24005d68a7bd3035afc21b76193945d6
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-30 23:02:46 -0500

    pushing

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,10 @@
-module.exports = (req, res, next) => {
-    // this checkes the cookie with the login data, and if it exist the it will go to next()
-    if (req.sessions && req.sessions.user) {
-        next()
-    } else {
-        try{
-            res.redirect('/')
-        } catch {
-            res.status(401).json({
-                message: 'Sorry you can not proceed'
+module.exports = () => {
+    return (req, res, next) => {
+      if (!req.session || !req.session.user) {
+        return res.status(403).json({
+              message: "your are not authorized",
             })
-        }
+          }
+        next()  
     }
-}
+  }    
