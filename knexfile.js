63e31141db3f24e89193166d3246fad4ed377904
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-02-03 21:39:03 -0500

    updating pg

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,23 @@
 module.exports = {
 
-  dev: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/weight.db3'
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done)
-      },
-    },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
+  // dev: {
+  //   client: 'sqlite3',
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: './data/weight.db3'
+  //   },
+  //   pool: {
+  //     afterCreate: (conn, done) => {
+  //       conn.run("PRAGMA foreign_keys = ON", done)
+  //     },
+  //   },
+  //   migrations: {
+  //     directory: "./data/migrations",
+  //   },
+  //   seeds: {
+  //     directory: "./data/seeds",
+  //   },
+  // },
   test: {
     client: 'sqlite3',
     useNullAsDefault: true,
@@ -32,29 +32,34 @@ module.exports = {
     },
 },
 
-staging: {
-  client: 'pg',
-  connection: {
-    filename: './data/weight.db3'
-  },
-  pool: {
-    afterCreate: (conn, done) => {
-      conn.run('PRAGMA foreign_keys = ON', done);
-    },
-  },
-  migrations : {
-    directory: './data/migrations'
-  },
-  seeds: {
-    directory: './data/seeds'
-  },
-},
+// staging: {
+//   client: 'pg',
+//   connection: {
+//     filename: './data/weight.db3'
+//   },
+//   pool: {
+//     afterCreate: (conn, done) => {
+//       conn.run('PRAGMA foreign_keys = ON', done);
+//     },
+//   },
+//   migrations : {
+//     directory: './data/migrations'
+//   },
+//   seeds: {
+//     directory: './data/seeds'
+//   },
+// },
 
 production: {
   client: 'pg',
   connection: process.env.DATABASE_URL,
+  pool: {
+    min: 2,
+    max: 10
+  },
   migrations : {
-    directory: './data/migrations'
+    directory: './data/migrations',
+    tableName: 'dbmigrations',
   },
   seeds: {
     directory: './data/seeds'
