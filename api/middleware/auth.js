30cbef720860321fb43e9f266b30bcfbc1ecedd9
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-02-04 08:18:45 -0500

    updated auth middleware

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken');
+const secrets = require('../auth/secrets')
 
 
 const authenticated = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+    jwt.verify(token, process.env.JWT_SECRET, secrets.jwtsecret, (err, decodedToken) => {
       if (err) {
-        console.log(err)
-        res.status(401).end()
-      } else {
-        req.decodeJwt = decodedToken;
+        // console.log(err)
+        res.status(401).json({message: "Unauthorized"})
+      } else {       
         next();
       }
     })
   } else {
-    res.status(401).end()
+    res.status(401).json({message: 'Must be logged in'})
   }
 }
 
