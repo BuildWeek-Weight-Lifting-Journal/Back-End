b3e16d913d909c0a8d2f1ad5e63b78582b6b499a
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-02-04 17:15:46 -0500

    update more documentation

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const usersRouter = require('./api/user/user-route');
 const exerciseRouter = require('./api/excersies/exercises-router');
 const journalRouter = require('./api/journals/journal-router');
 const jouexeRouter = require('./api/journalsExcersise/je-router')
-
+const auth = require('./api/auth/auth-router')
 
 
 const port = process.env.PORT || 5000
@@ -21,11 +21,11 @@ server.use(helmet())
 server.use(cors())
 server.use(express.json())
 
-server.use('/api/auth', authRouter); 
-server.use('/api/users', usersRouter)
-server.use('/api/exercises', exerciseRouter);
-server.use('/api/journals', journalRouter);
-server.use('/api/jouexe', jouexeRouter);
+server.use('/api/auth',  authRouter); 
+server.use('/api/users', auth, usersRouter)
+server.use('/api/exercises', auth, exerciseRouter);
+server.use('/api/journals', auth, journalRouter);
+server.use('/api/jouexe', auth, jouexeRouter);
 
 server.get('/', (req, res) => {
     res.redirect('https://documenter.getpostman.com/view/9002882/SWTEdwUH?version=latest')
